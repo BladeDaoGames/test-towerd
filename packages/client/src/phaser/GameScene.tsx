965f089b6309f7db7d45b80fd356acb7af3a2b73
0xlonescv <0xbuilderham@gmail.com>
2023-11-27 12:17:37 +0800

    :sparkles: Add moving creeps

--- packages/client/src/phaser/GameScene.tsx
+++ packages/client/src/phaser/GameScene.tsx
@@ -1,6 +1,11 @@
 import Phaser from 'phaser';
+import Enemy  from './enemies/Enemies';
 
 class GameScene extends Phaser.Scene {
+
+    private enemies!: Phaser.Physics.Arcade.Group;
+    private nextEnemy!: number;
+    private path!: Phaser.Curves.Path;
 
     constructor(){
         super('GameScene')
@@ -17,18 +22,33 @@ class GameScene extends Phaser.Scene {
 
         // the path for our enemies
         // parameters are the start x and y of our path
-        const path = this.add.path(96, -32);
-        path.lineTo(96, 164);
-        path.lineTo(480, 164);
-        path.lineTo(480, 544);
+        this.path = this.add.path(96, -32);
+        this.path.lineTo(96, 164);
+        this.path.lineTo(480, 164);
+        this.path.lineTo(480, 544);
 
         graphics.lineStyle(3, 0xffffff, 1);
         // visualize the path
-        path.draw(graphics);
+        this.path.draw(graphics);
+
+        this.enemies = this.physics.add.group({ classType: Enemy, runChildUpdate: true });
+        this.nextEnemy = 0;
     }
 
-    update() {
+    update(time:number, delta:number) {
         //
+        if (time > this.nextEnemy)
+        {
+            const enemy = this.enemies.get();
+            if (enemy)
+            {
+                enemy.setActive(true);
+                enemy.setVisible(true);
+                enemy.startOnPath(this.path);
+
+                this.nextEnemy = time + 2000;
+            }       
+        }
     }
 
 }
---
+++ packages/client/src/phaser/enemies/Enemies.ts
@@ -0,0 +1,52 @@
+import Phaser from 'phaser';
+import { ENEMY_SPEED } from '../constants';
+
+export default class Enemy extends 
+    Phaser.GameObjects.Image {
+
+        private follower: { t: number, vec: Phaser.Math.Vector2 };
+        hp: number;
+        path: Phaser.Curves.Path;
+
+        constructor(scene: Phaser.Scene, x: number, y: number,
+            texture: string, frame: string | number) {
+                super(scene, x, y, "sprites", "enemy");
+
+                this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
+                this.hp = 0;
+                //this.path = path;
+            }
+
+        startOnPath(path: Phaser.Curves.Path) {
+            this.follower.t = 0;
+            this.hp = 100;
+            this.path= path;
+            this.path.getPoint(this.follower.t, this.follower.vec);
+            
+            this.setPosition(this.follower.vec.x, this.follower.vec.y);
+        }
+
+        receiveDamage(damage: number) {
+            this.hp -= damage;           
+            
+            // if hp drops below 0 we deactivate this enemy
+            if(this.hp <= 0) {
+                this.setActive(false);
+                this.setVisible(false);      
+            }
+        }
+
+        update(time: number, delta: number) {
+            //
+            this.follower.t += ENEMY_SPEED * delta;
+            this.path.getPoint(this.follower.t, this.follower.vec);
+            
+            this.setPosition(this.follower.vec.x, this.follower.vec.y);
+
+            if (this.follower.t >= 1)
+            {
+                this.setActive(false);
+                this.setVisible(false);
+            }
+        }
+    }
